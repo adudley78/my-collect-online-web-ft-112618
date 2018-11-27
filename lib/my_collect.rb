f7efbfcf5a6c001af8d7d323de0291e64a244f2c
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 23:58:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
-def my_collect(collection)
-  i = 0
-  list_stuff = [ ]
+def my_collect(collection) # defines a new method called my_collect that has a parameter placeholder called collection
+  i = 0  #sets the while loop counter equal to 0
+  list_stuff = [ ] #creates an empty array called list_stuff where the iterated over array of items will be stored
   while i < collection.length
     list_stuff << yield(collection[i])
     i += 1
