7ab1947964762d1094ec5f3786022f36b09e880d
Adam Dudley <dudleyadam@gmail.com>
2018-11-27 23:55:58 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+require 'pry'
+
+def my_collect(collection)
+  i = 0
+  list_stuff = [ ]
+  while i < collection.length
+    list_stuff << yield(collection[i])
+    i += 1
+    #my_collect([list_stuff]) do |item|
+      #if item.downcase
+      #  item.upcase
+      #  end
+   end
+   list_stuff
+end
 
 
+#my_collect([ ]) do |item|
+#  if item.downcase(" ")
+#    puts item.upcase
+#  end
+#end
