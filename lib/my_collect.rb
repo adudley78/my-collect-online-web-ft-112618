e376591749f3bbc96e604c720aa9c66035b66cb8
Adam Dudley <dudleyadam@gmail.com>
2018-11-28 00:04:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_collect(collection) # defines a new method called my_collect that has a parameter placeholder called collection
   i = 0  #sets the while loop counter equal to 0
   list_stuff = [ ] #creates an empty array called list_stuff where the iterated over array of items will be stored
-  while i < collection.length #invokes a while loop that runs as long as i is less than the size of the array passed in is 
-    list_stuff << yield(collection[i]) #for each item in the array passed in, shovel it into a new variable placeholder called List_stuff
+  while i < collection.length #invokes a while loop that runs as long as i is less than the size of the array passed in is
+    list_stuff << yield(collection[i]) #yield each item in the array passed in to the block and shovel it into a new variable placeholder called List_stuff
     i += 1 #increment the value of i, or step up the value i by 1 with each loop
    end
-   list_stuff #return the value(s) of a new array, list_stuff
+   list_stuff #return the value(s) of the new array called list_stuff
 end
